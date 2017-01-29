7f1596628402805763b750b41aea690a9d436f4d
gas <gasweld@gmail.com>
2017-01-29 04:53:39 +0300

    Drivers table -> React

--- panels/.src/disp/app/src/components/drivers-table.js
+++ panels/.src/disp/app/src/components/drivers-table.js
@@ -1,15 +1,37 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-import DriversTableWidget from '../widgets/drivers-table.js';
-
 export default class DriversTable extends React.Component {
-	componentDidMount() {
-		var dw = new DriversTableWidget(this.props.client);
-		ReactDOM.findDOMNode(this).firstChild.appendChild(dw.root());
-	}
-
 	render() {
-		return <div className="drivers-table"><div></div></div>;
+		var disp = this.props.client;
+		return (
+			<table className="items">
+			<thead>
+				<tr><th>Позывной</th><th>Имя</th><th>Телефон</th><th>Автомобиль</th><th>Номер</th><th>Цвет</th></tr>
+			</thead>
+			<tbody>
+				{disp.drivers().map(d => <Row key={d.id} client={disp} driver={d} />)}
+			</tbody>
+			</table>
+		);
+	}
+};
+
+class Row extends React.Component {
+	render() {
+		var d = this.props.driver;
+		var disp = this.props.client;
+		var c = disp.getCar( d.car_id );
+		if( !c ) c = {name: "", plate: "", color: ""};
+		return (
+			<tr>
+			<td>{d.call_id}</td>
+			<td>{d.name}</td>
+			<td>{d.phone}</td>
+			<td>{c.name}</td>
+			<td>{c.plate}</td>
+			<td>{c.color}</td>
+			</tr>
+		);
 	}
 };
