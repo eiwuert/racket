ae637150cc9ecd7f5ebd32a48a7cc578f6513e5b
gas <gasweld@gmail.com>
2017-01-31 20:22:56 +0300

    Customer section partial rewrite

--- panels/.src/disp/app/src/order-form/customer.js
+++ panels/.src/disp/app/src/order-form/customer.js
@@ -1,112 +1,185 @@
 import {formatPhone} from '../../lib/format.js';
 import Dialog from '../../lib/dialog.js';
 
-export default function CustomerSection( $container )
-{
-	var ids = Date.now();
-	var s = '<div><label for="id1">Телефон клиента</label>'
-		+ '<input type="tel" id="id1"></div>'
-		+ '<div><label for="id2">Имя клиента</label><input id="id2"><button class="history" type="button" title="Адреса">Адреса</button>';
-	s = s.replace( 'id1', '--id-cust-' + (++ids) );
-	s = s.replace( 'id2', '--id-cust-' + (++ids) );
-	var $s = $( s );
-	$container.append( $s );
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-	var $phone = $s.find( "input" ).eq(0);
-	var $name = $s.find( "input" ).eq(1);
-	var $button = $s.find( "button" ).eq(0);
-	$button.prop( "disabled", true );
+class CustomerInput extends React.Component {
+	phoneChange(e) {
+		this.props.onPhoneChange(e.target.value);
+	}
+	nameChange(e) {
+		this.props.onNameChange(e.target.value);
+	}
 
-	this.get = function() {
-		return {
-			customer_phone: getPhone(),
-			customer_name: $name.val()
+	render() {
+		return (<div>
+			<div>
+				<label>Телефон клиента</label>
+				<input type="tel" value={this.props.phone} onChange={this.phoneChange.bind(this)} />
+			</div>
+			<div>
+				<label>Имя клиента</label>
+				<input
+					className={this.props.wait? 'wait' : ''}
+					value={this.props.name} onChange={this.nameChange.bind(this)} />
+			</div>
+		</div>);
+	}
+};
+
+class RCustomerSection extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			addresses: [],
+			wait: 0
 		};
-	};
-
-	var addresses = [];
-	var onAddress = null;
-
-	this.onAddress = function( func ) {
-		onAddress = func;
-	};
-
-	$button.on( "click", function() {
-		var s = '<div class="menu">';
-		addresses.forEach( function( addr, i ) {
-			s += '<div data-id="'+i+'">'+addr.format()+'</div>';
+		this.nameChange = this.nameChange.bind(this);
+		this.phoneChange = this.phoneChange.bind(this);
+	}
+	
+	nameChange(name) {
+		this.props.onNameChange(name);
+	}
+	
+	phoneChange(phone) {
+		this.props.onPhoneChange(phone);
+		
+		var t = this;
+		
+		t.setState(function(s) {
+			s.wait++;
+			s.addresses = [];
+			return s;
 		});
-		s += '</div>';
-
-		var $c = $( s );
-		var d = new Dialog( $c.get(0) );
-		d.setTitle( "Адреса клиента" );
-		d.addButton( "Закрыть", null, "no" );
-		d.show();
-
-		$c.on( "click", "div", function() {
-			var i = $(this).data( "id" );
-			if( typeof i == "undefined" ) return;
-			if( onAddress ) onAddress( addresses[i] );
-			d.close();
-		});
-	});
-
-	$phone.on( "change", function() {
-		var phone = getPhone();
-		$name.addClass( "wait" );
-		$button.prop( "disabled", true );
-		disp.findCustomer( phone ).then( function( customer ) {
-			$name.val( customer.name );
-			$name.removeClass( "wait" );
-			addresses = customer.addresses;
-			if( addresses.length > 0 ) {
-				$button.prop( "disabled", false );
-			}
+	
+		disp.findCustomer( getPhone(phone) ).then( function( customer ) {
+			t.props.onNameChange(customer.name);
+			t.setState(function(s) {
+				s.wait--;
+				s.addresses = customer.addresses;
+				return s;
+			});
 		})
 		.catch( function() {
 			// No such customer
-			$name.removeClass( "wait" );
+			t.setState(function(s) {
+				s.wait--;
+				return s;
+			});
 		});
-	});
-
-	function getPhone()
-	{
-		var n = $phone.val().replace( /[\s]/g, '' );
-		/*
-		 * If there is not enough digits even for bare number, return
-		 * whatever is there.
-		 */
-		if( n.length < 7 ) return n;
-		/*
-		 * If it's a bare number, add default area code.
-		 * Also add country code if it's not there.
-		 */
-		if( n.length == 7 ) {
-			n = "29" + n;
-		}
-		if( n.length == 9 ) {
-			n = "+375" + n;
-		}
-		return n;
 	}
+
+	render() {
+		return (<div>
+			<CustomerInput name={this.props.name} phone={this.props.phone}
+				onPhoneChange={this.phoneChange}
+				onNameChange={this.nameChange}
+				wait={this.state.wait > 0}/>
+			<Addrlist list={this.state.addresses}
+				onAccept={this.props.onAddress}
+				/>
+		</div>);
+	}
+};
+
+class Addrlist extends React.Component {
+	render() {
+		return (<div>
+			{this.props.list.map((a, i) => <button key={i} type="button"
+				onClick={this.props.onAccept.bind(undefined, a)}>{a.format()}</button>)}
+		</div>);
+	}
+};
+
+export default function CustomerSection( $container )
+{
+	var c = document.createElement('div');
+	$container.append(c);
+	
+	var s = {
+		name: '',
+		phone: ''
+	};
+	
+	function r() {
+		ReactDOM.render(<RCustomerSection
+			name={s.name} phone={s.phone}
+			onPhoneChange={onPhoneChange}
+			onNameChange={onNameChange}
+			onAddress={onAddressAccept}
+			/>, c);
+	}
+	r();
+	
+	function onPhoneChange(phone) {
+		s.phone = phone;
+		s.name = '';
+		r();
+	}
+	
+	function onNameChange(name) {
+		s.name = name;
+		r();
+	}
+	
+	var onAddress = null;
+	function onAddressAccept(addr) {
+		onAddress(addr);
+	}
+
+	this.get = function() {
+		return {
+			customer_phone: s.phone,
+			customer_name: s.name
+		};
+	};
+	
+	this.onAddress = function( func ) {
+		onAddress = func;
+	};
 
 	this.set = function( order )
 	{
 		var phone = order.customer_phone;
 		var name = order.customer_name;
 		if( phone ) {
-			$phone.val( formatPhone( phone ) );
+			phone = formatPhone( phone );
 		}
-		$name.val( name );
+		s.phone = phone;
+		s.name = name;
+		r();
 	};
 
 	this.setPhone = function( phone, trigger )
 	{
-		$phone.val( phone );
-		$name.val( '' );
+		s.phone = phone;
+		s.name = '';
+		r();
 		if( trigger ) {
-			$phone.trigger( 'change' );
+			console.warn("Can't trigger phonechange");
 		}
 	};
+}
+
+function getPhone(val)
+{
+	var n = val.replace( /[\s]/g, '' );
+	/*
+	 * If there is not enough digits even for bare number, return
+	 * whatever is there.
+	 */
+	if( n.length < 7 ) return n;
+	/*
+	 * If it's a bare number, add default area code.
+	 * Also add country code if it's not there.
+	 */
+	if( n.length == 7 ) {
+		n = "29" + n;
+	}
+	if( n.length == 9 ) {
+		n = "+375" + n;
+	}
+	return n;
 }
