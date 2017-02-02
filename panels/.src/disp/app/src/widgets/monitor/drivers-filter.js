5b1f27d6daa4f11f3419b04110b904ee931e3516
gas <gasweld@gmail.com>
2017-02-02 16:38:23 +0300

    Drivers filter rewrite

--- panels/.src/disp/app/src/widgets/monitor/drivers-filter.js
+++ panels/.src/disp/app/src/widgets/monitor/drivers-filter.js
@@ -1,80 +1,137 @@
-export default function DriversFilterWidget( disp )
+var React = require('react'), ReactDOM = require('react-dom');
+var _ = require('underscore');
+
+function add(arr, val) {
+	var i = arr.indexOf(val);
+	if (i == -1) {
+		arr.push(val);
+	}
+	return arr;
+}
+
+function remove(arr, val) {
+	var i = arr.indexOf(val);
+	if (i != -1) {
+		arr.splice(i, 1);
+	}
+	return arr;
+}
+
+class DriversFilter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.change = this.change.bind(this);
+	}
+
+	change(e) {
+		var s = _.clone(this.props.state);
+		var name = e.target.name;
+		var on = e.target.checked;
+
+		if (name == 'term') {
+			s.term = on;
+			this.props.onChange(s);
+			return;
+		}
+
+		var id = parseInt(e.target.value);
+		if (on) {
+			s[name] = add(s[name], id);
+		} else {
+			s[name] = remove(s[name], id);
+		}
+		this.props.onChange(s);
+	}
+
+	render() {
+		var s = this.props.state;
+		var t = this;
+
+		return (<div id="cars-selector">
+			<b>Выбор водителей</b>
+			<span>
+				<label><input
+					type="checkbox" name="term"
+					checked={s.term}
+					onChange={t.change}/> терминал</label>
+			</span>
+			<span>
+			{
+				disp.driverTypes().map(function(type, i) {
+					var checked = s.types.indexOf(type.type_id) != -1;
+
+					return (<label key={i}><input
+						type="checkbox" name="types"
+						value={type.type_id}
+						checked={checked}
+						onChange={t.change}/> {type.name}</label>);
+				})
+			}
+			</span>
+			<span>
+			{
+				disp.driverGroups().map(function(group, i) {
+					var checked = s.groups.indexOf(group.group_id) != -1;
+					return <label key={i}><input
+						type="checkbox"
+						name="groups"
+						value={group.group_id}
+						checked={checked}
+						onChange={t.change}
+						/> {group.name}</label>;
+				})
+			}
+			</span>
+		</div>);
+	}
+};
+
+export default function DriversFilterWidget(disp)
 {
-	var s = '<div id="cars-selector">\
-		<b>Выбор водителей:</b>\
-		<span>\
-		<input type="checkbox" id="cb-terminal-highlight">\
-		<label for="cb-terminal-highlight">терминал</label>\
-		</span>';
+	var s = {
+		term: false,
+		types: [],
+		groups: []
+	};
 
-	var types = disp.driverTypes();
-	if( types.length > 0 )
-	{
-		s += '<span>';
-		types.forEach( function( type ) {
-			var id = "r-filter-type-" + type.type_id;
-			s += '<input type="checkbox" id="'+id+'" name="type" '
-				+ 'value="'+type.type_id+'">' +
-				'<label for="'+id+'">'+type.name+'</label>';
-		});
-		s += '<span>';
-	}
-
-	var groups = disp.driverGroups();
-	if( groups.length > 0 )
-	{
-		s += '<span>';
-		groups.forEach( function( group ) {
-			var id = "r-filter-group-" + group.group_id;
-			s += '<input type="checkbox" id="'+id+'" name="group" '
-				+ 'value="'+group.group_id+'">' +
-				'<label for="'+id+'">'+group.name+'</label>';
-		});
-		s += '<span>';
-	}
-
-	s += '</div>';
-
-	var $c = $( s );
-	var $term = $c.find( '#cb-terminal-highlight' );
-	$term.on( 'change', applyFilter );
-
-	var $type = $c.find( 'input[name="type"]' );
-	$type.on( 'change', applyFilter );
-
-	var $group = $c.find( 'input[name="group"]' );
-	$group.on( 'change', applyFilter );
-
+	var root = document.createElement('div');
 	this.root = function() {
-		return $c.get(0);
+		return root;
 	};
 
 	var listeners = [];
-	this.onChange = listeners.push.bind( listeners );
+	this.onChange = listeners.push.bind(listeners);
 
-	this.clear = function() {
-		$term.prop( "checked", false );
-		$type.prop( "checked", false );
-		$group.prop( 'checked', false );
-	};
+	function r() {
+		ReactDOM.render(<DriversFilter state={s} onChange={onChange}/>, root);
+	}
+	r();
 
-	function applyFilter()
-	{
+	function onChange(newState) {
+		s = newState;
+		r();
+
 		var filter = [];
-		if( $term.is( ':checked' ) ) {
-			filter.push( {has_bank_terminal: 1} );
+		if (s.term) {
+			filter.push({has_bank_terminal: 1});
 		}
 
-		$type.filter( ":checked" ).each( function() {
-			filter.push( {type_id: this.value} );
+		s.types.forEach(function(typeId) {
+			filter.push({type_id: typeId});
 		});
 
-		$group.filter( ":checked" ).each( function() {
-			filter.push( {group_id: this.value} );
+		s.groups.forEach(function(groupId) {
+			filter.push({group_id: groupId});
 		});
 
-		listeners.forEach( function( f ) {
-			f( filter );
+		listeners.forEach(function(f) {
+			f(filter);
 		});
 	}
+
+	this.clear = function() {
+		s = {term: false, types: [], groups: []};
+		r();
+	};
+
 }
