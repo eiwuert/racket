880c346a39db12b60f7dc3f71eeb0ea43e734439
gas <gasweld@gmail.com>
2017-02-03 16:02:02 +0300

    Move close session dialog in

--- panels/.src/disp/app/src/components/sessions-table.js
+++ panels/.src/disp/app/src/components/sessions-table.js
@@ -1,5 +1,5 @@
 import {formatDateTime} from '../../lib/format.js';
-import AppDialog from './app-dialog.js';
+import Dialog from './dialog.js';
 var React = require('react');
 var ReactDOM = require('react-dom');
 
@@ -8,11 +8,13 @@ export default class SessionsTable extends React.Component {
 		super(props);
 		var disp = props.client;
 		this.state = {
-			sessions: disp.sessions()
+			sessions: disp.sessions(),
+			closeDialog: false
 		};
 		this.refresh = this.refresh.bind(this);
+		this.hideCloseDialog = this.hideCloseDialog.bind(this);
 	}
-	
+
 	refresh() {
 		this.setState({sessions: this.props.client.sessions()});
 	}
@@ -25,6 +27,28 @@ export default class SessionsTable extends React.Component {
 		this.props.client.events.off('sessions-changed', this.refresh);
 	}
 
+	onCloseClick(s) {
+		var disp = this.props.client;
+		var driver = disp.getDriver( s.driver_id );
+		/*
+		 * If the driver is an imitation, close the session without
+		 * asking.
+		 */
+		if( driver.is_fake == '1' ) {
+			disp.closeSession( driver.id, 0 );
+			return;
+		}
+		/*
+		 * If the driver is real, ask for the odometer value before
+		 * closing.
+		 */
+		this.setState({closeDialog: driver});
+	}
+
+	hideCloseDialog() {
+		this.setState({closeDialog: null});
+	}
+
 	render() {
 		return (<div className="sessions-table">
 			<button type="button" className="btn btn-default" onClick={this.props.onNewClick}>Открыть смену</button>
@@ -34,42 +58,29 @@ export default class SessionsTable extends React.Component {
 					<th>Машина</th>
 					<th></th></tr>
 				</thead>
-				<tbody>
-					{ disp.sessions().map(s => <Row key={s.session_id} session={s} client={this.props.client}/>) }
-				</tbody>
+				<tbody>{
+					disp.sessions().map(s => <Row
+						client={this.props.client}
+						key={s.session_id}
+						session={s}
+						onCloseClick={this.onCloseClick.bind(this)}/>)
+				}</tbody>
 			</table>
+			{this.state.closeDialog &&
+				<CloseSessionDialog
+					client={this.props.client}
+					driver={this.state.closeDialog}
+					onAccept={this.hideCloseDialog}
+					onDecline={this.hideCloseDialog}/>
+			}
 		</div>);
 	}
 };
 
 class Row extends React.Component {
-	click(event) {
-		var driver_id = this.props.session.driver_id;
-		var button = event.target;
-		var disp = this.props.client;
-
-		var d = disp.getDriver( driver_id );
-		/*
-		 * If the driver is an imitation, close the session without
-		 * asking.
-		 */
-		if( d.is_fake == '1' ) {
-			button.disabled = true;
-			disp.closeSession( driver_id, 0 );
-			return;
-		}
-		/*
-		 * If the driver is real, ask for the odometer value before
-		 * closing.
-		 */
-		var driver = disp.getDriver( driver_id );
-		if( !driver ) {
-			console.error( "Unknown driver id: ", driver_id );
-			return;
-		}
-		window.__open(<CloseSessionDialog client={disp} driver={d} />, 'close-session-' + driver_id);
+	click() {
+		this.props.onCloseClick(this.props.session);
 	}
-
 	render() {
 		var s = this.props.session;
 		var disp = this.props.client;
@@ -84,25 +95,26 @@ class Row extends React.Component {
 	}
 };
 
-
 class CloseSessionDialog extends React.Component {
 	accept() {
 		var $s = $(ReactDOM.findDOMNode(this));
 		var odometer = $s.find( 'input' ).val();
-		return this.props.client.closeSession( this.props.driver.id, odometer );
+		this.props.client.closeSession( this.props.driver.id, odometer );
+		this.props.onAccept();
 	}
 	render() {
-		return (<AppDialog
+		return (<Dialog
 			id={this.props.id}
 			title={'Закрытие смены для ' + this.props.driver.call_id}
 			yes="Закрыть"
 			no="Отменить"
-			onAccept={this.accept.bind(this)}>
+			onAccept={this.accept.bind(this)}
+			onDecline={this.props.onDecline}>
 			<div className="form-group">
 				<label>Одометр</label>
 				<input className="form-control" type="number" min="0" step="1" />
 			</div>
-		</AppDialog>);
+		</Dialog>);
 	}
 };
 
