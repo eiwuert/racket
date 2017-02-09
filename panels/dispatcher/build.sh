1c6f6fff28ed41cc92ab8f274df5a16da3842774
gas <gasweld@gmail.com>
2017-02-09 18:22:56 +0300

    Convert PHP+CSS to LESS

--- panels/dispatcher/build.sh
+++ panels/dispatcher/build.sh
@@ -4,10 +4,7 @@ dest=../www/res/dispatcher
 
 
 # build the stylesheet
-	cat css/main.css.php css/*.css > tmp.php
-	php tmp.php > $dest/order.css
-	lessc css/app.less >> $dest/order.css
-	rm tmp.php
+	lessc style.less >> $dest/order.css
 
 # build the order script
 	gulp
