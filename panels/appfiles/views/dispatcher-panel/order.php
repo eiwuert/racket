c7e56ee032bcc0cc43382173fc558c54deab8edb
gas <gasweld@gmail.com>
2017-02-12 01:48:27 +0300

    Dispatcher's view cleanup

--- panels/appfiles/views/dispatcher-panel/order.php
+++ panels/appfiles/views/dispatcher-panel/order.php
@@ -26,16 +26,6 @@ require_stylesheet('res/lib/jquery-ui/jquery-ui.min.css');
 </head>
 <body>
 
-<header>
-	<nav>
-		<h1>Диспетчер <?= user::get_login() ?></h1>
-		<ul>
-			<li><a href="<?= url( 'orders' ) ?>">Архив заказов</a></li>
-			<li><a href="<?= aurl( 'logout' ) ?>">Выход</a></li>
-		</ul>
-	</nav>
-</header>
-
 <div id="app-container"></div>
 
 </body>
