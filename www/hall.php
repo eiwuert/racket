ecf8ff12c4b5aa32c655ed03f63d27a28bba8923
gas <gasweld@gmail.com>
2017-02-17 17:37:55 +0300

    Removed old framework

--- www/hall.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require '../panels/appfiles/init.php';
-
-?>
--- www/index.php
+++ www/index.php
@@ -1,38 +1,3 @@
 <?php
-
-/*
- * Right now we need the two frameworks to coexist.
- * Rather than mess with Apache's mod_rewrite,
- * dispatch the requests from here.
- */
-
-function serve_sym()
-{
-	$sym = [
-		'/sym/',
-		'/dx/dispatcher/',
-		'/_wdt/',
-		'/_profiler/',
-		'/dispatcher',
-		'/service',
-		'/json'
-	];
-
-	$url = $_SERVER['REQUEST_URI'];
-	if (substr($url, -1) != '/') {
-		$url .= '/';
-	}
-
-	foreach ($sym as $pref) {
-		if (strpos($url, $pref) === 0) {
-			require 'app.php';
-			return true;
-		}
-	}
-	return false;
-}
-
-if (!serve_sym()) {
-	require 'hall.php';
-}
+require 'app.php';
 ?>
